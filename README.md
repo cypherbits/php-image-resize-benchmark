b44138e6f60c127aaa8a0a1e0cb4ed4519096a20
cypherbits <info@avanix.es>
2021-04-18 17:45:47 +0200

    Create repo

---
+++ README.md
@@ -0,0 +1,33 @@
+## Benchmarking image thumbnail generation in PHP 8
+#### Original benchmark script from https://robertvicol.com/tech/imagemagick-vs-gd-benchmark-resize-image-in-php-script/
+
+We are testing on PHP8 this libraries:
+- php8.0-imagick
+- php8.0-gd
+- php8.0-vips
+
+They say on GD vs Imagick, Imagick always is faster. Not on my machine. CPU i7-4702MQ.
+
+Benchmark results:
+
+- ImageMagick took: 0.37198996543884 seconds. 103 KB.
+- GD took: 0.29470610618591 seconds. 164 KB.
+- VIPS took: 0.083019018173218 seconds. 53 KB.
+
+Example images:
+
+Original
+
+![Original](benchmark-img.jpg)
+
+ImageMagick
+
+![Test Imagick](test-imagick.jpg)
+
+GD
+
+![Test GD](test-gd.jpg)
+
+VIPS
+
+![Test VIPS](test-vip.jpg)
---
+++ benchmark.php
@@ -0,0 +1,72 @@
+<?php
+require_once "vendor/autoload.php";
+
+$fsource = "benchmark-img.jpg";
+$fdest_imagick = "test-imagick.jpg";
+$fdest_gd = "test-gd.jpg";
+$fdest_vip = "test-vip.jpg";
+
+$w = 640;
+$h = 640;
+
+// ~~~~~~~~~~~~~~~~~ ImageMagick ~~~~~~~~~~~~~~~~~~~~~
+
+$start_time = microtime(true);
+
+$im = new imagick( $fsource );
+$im->setImageCompression(Imagick::COMPRESSION_JPEG);
+$im->setImageCompressionQuality(90);
+$im->stripImage();
+$im->thumbnailImage($w, 0);
+//$im->resizeImage($w,$h,Imagick::FILTER_LANCZOS,1);
+$im->writeImage( $fdest_imagick );
+
+$end_time = microtime(true);
+
+echo ($end_time-$start_time) . " sec";
+echo "<br />";
+echo "<img src='$fdest_imagick'>";
+
+echo "<hr />";
+
+// ~~~~~~~~~~~~~~~~~~ GD php library ~~~~~~~~~~~~~~~~~~~~
+
+$start_time = microtime(true);
+
+
+list($width, $height) = getimagesize( $fsource );
+$r = $width / $height;
+
+if ($w/$h > $r) {
+    $newwidth = $h*$r;
+    $newheight = $h;
+} else {
+    $newheight = $w/$r;
+    $newwidth = $w;
+}
+
+$src = imagecreatefromjpeg($fsource);
+$dst = imagecreatetruecolor($newwidth, $newheight);
+imagecopyresampled($dst, $src, 0, 0, 0, 0, $newwidth, $newheight, $width, $height);
+
+imagejpeg($dst, $fdest_gd, 90 );
+
+$end_time = microtime(true);
+
+echo ($end_time-$start_time) . " sec";
+echo "<br />";
+echo "<img src='$fdest_gd'>";
+
+// ~~~~~~~~~~~~~~~~~~ VIPs php library ~~~~~~~~~~~~~~~~~~~~
+
+$start_time = microtime(true);
+
+$im = Jcupitt\Vips\Image::thumbnail($fsource, $w, ['height' => $h]);
+$im->writeToFile($fdest_vip);
+
+
+$end_time = microtime(true);
+
+echo ($end_time-$start_time) . " sec";
+echo "<br />";
+echo "<img src='$fdest_vip'>";
